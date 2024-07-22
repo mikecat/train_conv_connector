b4e81820d543c3f64635d9a9060c1fc3b876fa18
MikeCAT <mikecat@users.noreply.github.com>
2024-07-22 22:49:53 +0900

    add AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyProduct("TrainConvConnector")]
+[assembly: AssemblyTitle("TRAIN CREWと電車でＧｏ！コントローラー変換器の橋渡し役")]
+[assembly: AssemblyCompany("みけCAT")]
+[assembly: AssemblyCopyright("Copyright (c) 2024 みけCAT")]
--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ OPTIONS= \
 	/reference:TrainCrewInput.dll
 
 SOURCES= \
+	AssemblyInfo.cs \
 	TrainConvConnector.cs \
 	UIText.cs \
 	JapaneseUIText.cs \
